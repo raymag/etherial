b2ffa990b001d7cc0b8c7751624a1dbe1877a89d
raymag <prayk11@outlook.com>
2021-07-24 14:41:29 -0300

    feat: testing

--- src/app/components/weather-board/weather-board.component.ts
+++ src/app/components/weather-board/weather-board.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { environment } from 'src/environments/environment.final';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-weather-board',
